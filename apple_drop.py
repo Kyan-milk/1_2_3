d61f89a6009d118388393fe47cb6f07aca65f880
Kyan-milk <91714831+Kyan-milk@users.noreply.github.com>
2021-11-19 12:56:22 -0800

    Update apple_drop.py

--- apple_drop.py
+++ apple_drop.py
@@ -4,19 +4,11 @@ import random as rand
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
-apple_image2 = "apple.gif"
-apple_image3 = "apple.gif"
-apple_image4 = "apple.gif"
-apple_image5 = "apple.gif"
 
 wn = trtl.Screen()
 wn.bgpic("background.gif")
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-wn.addshape(apple_image2)
-wn.addshape(apple_image3)
-wn.addshape(apple_image4)
-wn.addshape(apple_image5)
 
 apple = trtl.Turtle()
 
@@ -26,22 +18,6 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
-def draw_apple2(active_apple):
-  active_apple.shape(apple_image2)
-  wn.update()
-
-def draw_apple3(active_apple):
-  active_apple.shape(apple_image3)
-  wn.update()
-
-def draw_apple4(active_apple):
-  active_apple.shape(apple_image4)
-  wn.update()
-
-def draw_apple5(active_apple):
-  active_apple.shape(apple_image5)
-  wn.update()
-
 def drop_apple():
   apple.penup()
   apple.goto(rand.randint(-200, 200),-200)
@@ -49,10 +25,6 @@ def drop_apple():
 #-----function calls-----
 wn.tracer(True)
 draw_apple(apple)
-draw_apple2(apple)
-draw_apple3(apple)
-draw_apple4(apple)
-draw_apple5(apple)
 wn.tracer(False)
 
 wn.onkeypress(drop_apple, "space")
